fa7a93a65dd7aeaa1fcfc4f5241b7eeec887596f
Bruno de Araujo Chaves <achaves.bruno@gmail.com>
2020-10-24 21:21:31 -0300

    Adding option to change between calibration and drive mode

--- server.py
+++ server.py
@@ -1,24 +1,72 @@
-# import the necessary packages
-
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 from utils import VideoCamera
 
+import cv2 as cv
+
 app = Flask(__name__)
+video_camera = VideoCamera()
+is_rasp = False
+
+
+def driver_mode(camera):
+    while True:
+        img = camera.run()
+        if not is_rasp:
+            cv.imshow('frame', img)
+            key = cv.waitKey(1) & 0xFF
+            # if the `q` key was pressed, break from the loop
+            if key == ord("c"):
+                cv.destroyAllWindows()
+                return 'c'
+            elif key == ord('q'):
+                cv.destroyAllWindows()
+                return 'q'
+        else:
+            pass  # rasp commands to choice the run mode
+
+
+def gen(camera):
+    while True:
+        # get camera frame
+        frame = camera.run()
+        # encode OpenCV raw frame to jpg and displaying it
+        _, jpeg = cv.imencode('.jpg', frame)
+        frame = jpeg.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/shutdown')
+def shutdown_server():
+    print("[INFO] encerrando servidor flask ...")
+    shutdown = request.environ.get('werkzeug.server.shutdown')
+    if shutdown is None:
+        raise RuntimeError('Função indisponível!')
+    else:
+        shutdown()
+    return "Servidor encerrado!"
+
 
 @app.route('/')
 def index():
-    # rendering webpage
+    print('[INFO] rendering webpage')
     return render_template('index.html')
-def gen(camera):
-    while True:
-        #get camera frame
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(VideoCamera()),
+    print('[INFO] video feed')
+    return Response(gen(video_camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 if __name__ == '__main__':
-    # defining server ip address and port
-    app.run(host='0.0.0.0',port='5000', debug=True)
+    while True:
+        print('[INFO] modo direção')
+        cmd = driver_mode(video_camera)
+        if cmd == 'q':
+            break
+        # app.run(host='0.0.0.0', port='5000', debug=True)
+        print('[INFO] calibration mode')
+        app.run(threaded=True, debug=False, use_reloader=False, host="0.0.0.0", port=5000)
--- utils.py
+++ utils.py
@@ -1,11 +1,23 @@
 from imutils import face_utils
 import numpy as np
-import argparse
 import time
 import dlib
 import cv2 as cv
+
 from azure.iot.device import IoTHubDeviceClient, Message
 from datetime import datetime
+
+
+pallete = {
+    "red": (0, 33, 255),
+    "blue": (255, 125, 0),
+    "green": (61, 147, 77),
+    "white": (255, 255, 255),
+    "black": (0, 0, 0),
+    "yellow": (0, 233, 255),
+    "blue_bright": (189, 153, 0),
+    "gray": (59, 48, 43),
+}
 
 
 # compute and return the euclidean distance between the two points
@@ -54,12 +66,12 @@ def send_message(eventType):
     except:
         print("[Erro] Falha ao enviar a mensagem")
 
+
 # Cria instancia do client do iot hub
 CONNECTION_STRING = "HostName=eyetractor-hubiot.azure-devices.net;DeviceId=camera-01;SharedAccessKey=5pwN7TTPM7/V0bqXI+wEruGdCJi1o3h6QLEQRNwua+g="
 client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
 # Mensagem a ser enviada para o HubIot
 MSG_TXT = '{{"CameraId": "{CAMERAID}", "EventType": "{EVENT}", "EventDate": "{TODAY}" }}'
-
 
 EAR_THRESH = 0.3
 EAR_CONSEC_FRAMES = 16
@@ -71,7 +83,6 @@ DR_CONSEC_FRAMES = 16
 MODEL_FILE = "models/res10_300x300_ssd_iter_140000.caffemodel"
 PROTOTXT_FILE = "models/deploy.prototxt"
 
-
 print("[INFO] loading models")
 predictor = dlib.shape_predictor('models/shape_predictor_68_face_landmarks.dat')
 face_cascade = cv.CascadeClassifier('models/haarcascade_frontalface_default.xml')
@@ -81,7 +92,6 @@ net = cv.dnn.readNetFromCaffe(PROTOTXT_FILE, MODEL_FILE)
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-is_rasp = False
 
 class VideoCamera(object):
     def __init__(self):
@@ -94,29 +104,42 @@ class VideoCamera(object):
         self.alarm_on = False
         self.ear = 0
         self.dr = 0
+        self.clear_bar()
+        self.status_color = ['red', 'green']
 
-    
+    def clear_bar(self):
+        self.bar = np.ones((200, 800, 3)).astype('uint8')
+        self.bar[..., 0] *= 98  # 59
+        self.bar[..., 1] *= 50  # 48
+        self.bar[..., 2] *= 35  # 43
+        cv.putText(self.bar, "EYE TRACTOR", (10, 30),
+                   cv.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+
+    '''
     def __del__(self):
-        #releasing camera
+        # releasing camera
         self.cap.release()
+    '''
 
-
-    def get_frame(self):
+    def run(self, is_rasp=False):
 
         begin = time.time()
         ret, frame = self.cap.read()
-        orig = frame.copy()
         frame = cv.resize(frame, (400, 400))
+        orig = frame.copy()
+        self.clear_bar()
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
         # detect faces in the grayscale frame
         rects = face_cascade.detectMultiScale(gray, scaleFactor=1.1,
-                                            minNeighbors=5, minSize=(30, 30),
-                                            flags=cv.CASCADE_SCALE_IMAGE)
+                                              minNeighbors=5, minSize=(30, 30),
+                                              flags=cv.CASCADE_SCALE_IMAGE)
 
+        status = 0
         frontal_face_flag = False
         for (x, y, w, h) in rects:
             frontal_face_flag = True
+            self.dr = 0
             rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
             cv.rectangle(frame, (int(x), int(y)), (int(x + w), int(y + h)), (255, 0, 0), 2)
             shape = predictor(gray, rect)
@@ -138,6 +161,7 @@ class VideoCamera(object):
             if self.ear < EAR_THRESH:
                 self.counter_ear += 1
                 if self.counter_ear >= EAR_CONSEC_FRAMES:
+                    status = 1
                     if not self.alarm_on:
                         self.alarm_on = True
                         if is_rasp:
@@ -146,8 +170,6 @@ class VideoCamera(object):
                             GPIO.output(buzzer, GPIO.HIGH)
                             print("Beep")
                             None
-                    cv.putText(frame, "ALERTA DE FADIGA!", (10, 30),
-                            cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
             else:
                 self.counter_ear = 0
                 self.alarm_on = False
@@ -156,12 +178,10 @@ class VideoCamera(object):
                     print("no Beep")
                     # Reset buzzer and LEDs
                     None
-
-            cv.putText(frame, f"EAR: {np.round(self.ear, 2)}", (10, 60),
-                    cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
             break
 
         if not frontal_face_flag:
+            self.ear = 0
             (h, w) = frame.shape[:2]
             blob = cv.dnn.blobFromImage(cv.resize(frame, (300, 300)), 1.0,
                                         (300, 300), (104.0, 177.0, 123.0))
@@ -177,9 +197,9 @@ class VideoCamera(object):
                 (startX, startY, endX, endY) = box.astype("int")
 
                 cv.rectangle(frame, (startX, startY), (endX, endY),
-                            (0, 0, 255), 2)
+                             (0, 0, 255), 2)
 
-                face = frame[startY:startY+int((endY-startY)*SUP_FACE), startX:endX]
+                face = frame[startY:startY + int((endY - startY) * SUP_FACE), startX:endX]
                 face_gray = cv.cvtColor(face, cv.COLOR_BGR2GRAY)
                 eyes = eye_cascade.detectMultiScale(face_gray, scaleFactor=1.1,
                                                     minNeighbors=5, minSize=(30, 30),
@@ -190,12 +210,13 @@ class VideoCamera(object):
                     if i >= 2:
                         break
                     cv.rectangle(face, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-                    eyes_pos_x.append(ex + (ew/2))
+                    eyes_pos_x.append(ex + (ew / 2))
 
                 self.dr = distraction_ratio(eyes_pos_x)
                 if self.dr is None or self.dr < DISTRACTION_THRESH:
                     self.counter_dr += 1
                     if self.counter_dr >= DR_CONSEC_FRAMES:
+                        status = 2
                         if not self.alarm_on:
                             self.alarm_on = True
                             if is_rasp:
@@ -204,8 +225,6 @@ class VideoCamera(object):
                                 GPIO.output(buzzer, GPIO.HIGH)
                                 print("Beep")
                                 None
-                        cv.putText(frame, "ALERTA DE DISTRACAO!", (10, 30),
-                                cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
                 else:
                     self.counter_dr = 0
                     self.alarm_on = False
@@ -215,17 +234,26 @@ class VideoCamera(object):
                         # Reset buzzer and LEDs
                         None
 
-            cv.putText(frame, f"DR: {self.dr}", (10, 60),
-                    cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        if status == 0:
+            cv.putText(self.bar, "MOTORISTA ATENTO", (10, 180),
+                       cv.FONT_HERSHEY_SIMPLEX, 0.7, pallete['green'], 2)
+        elif status == 1:
+            cv.putText(self.bar, "ALERTA DE FADIGA!", (10, 180),
+                       cv.FONT_HERSHEY_SIMPLEX, 0.7, pallete['red'], 2)
+        elif status == 2:
+            cv.putText(self.bar, "ALERTA DE DISTRACAO!", (10, 180),
+                       cv.FONT_HERSHEY_SIMPLEX, 0.7, pallete['red'], 2)
 
-        # Show the frame
-        result = cv.resize(orig, (400, 400))
-        result = cv.hconcat((frame, result))
-        # encode OpenCV raw frame to jpg and displaying it
-        ret, jpeg = cv.imencode('.jpg', result)
+        cv.putText(self.bar, f"EAR: {np.round(self.ear, 2)}", (10, 90),
+                   cv.FONT_HERSHEY_SIMPLEX, 0.7, pallete[self.status_color[status == 0]], 2)
+
+        cv.putText(self.bar, f"DR: {self.dr}", (10, 130),
+                   cv.FONT_HERSHEY_SIMPLEX, 0.7, pallete[self.status_color[status == 0]], 2)
+
+        img_display = cv.hconcat((frame, orig))
+        result_display = cv.vconcat((img_display, self.bar))
         print(f'loop time = {np.round(time.time() - begin, 2)}')
-        return jpeg.tobytes()
-
+        return result_display
 
 
 if __name__ == '__main__':
